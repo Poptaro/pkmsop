c435a9d059d11e6fdddf8d7f04cd1caff51dda15
Poptaro <noahahn199@gmail.com>
2024-01-31 18:36:46 -0600

    smash/pass works and cycles through in order (1 - 151), no result function yet

--- index.js
+++ index.js
@@ -1,29 +1,88 @@
 
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("button").addEventListener('click', getPokemon)
-    var counter = 1
+    const startButton = document.getElementById("start");
+    const pokeNameBar = document.querySelector('.pokeName');
+    const pokemonPic = document.querySelector('.pokePic');
+    const smashButton = document.querySelector('#smash');
+    const passButton = document.querySelector('#pass');
 
+    let pokemonPicList = [];
+    let pokemonNameList = [];
+    let smashList = []
+    let passList = []
 
-    function getPokemon() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://pokeapi.co/api/v2/pokemon?limit=151', true);
-    
-        let listPkm = '';
-
-        xhr.onload = function () {
-            if(this.status == 200) {
-                var pokemon = JSON.parse(this.responseText);
-                for(let i in pokemon.results){
-                    let results = pokemon.results[i];   
-                    console.log(results.name);           
-  
-                    // listPkm -=`<li>${results.name}</li>`
-                }
-
-                // document.getElementById('lista').innerHTML = listPkm;
-            }
-        }
-        xhr.send();
-        
+    function uppercaseFirst (obj) {
+        return obj.charAt(0).toUpperCase() + obj.slice(1);
     }
+    async function fetchPokemon() {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        const responseJson = await response.json();
+        const responseResults = responseJson.results;
+
+
+        // Grabs all 151 pokemons default artwork and shoves them into the empty array
+
+
+        for(let i = 0; i < responseResults.length; i++) {
+            const pkm = await fetch(responseResults[i].url)
+            const pkmJson = await pkm.json();
+            const pkmFrontDefault = pkmJson.sprites.other['official-artwork'].front_default;
+            const pkmName = pkmJson.name;
+            pokemonPicList.push(pkmFrontDefault);
+            pokemonNameList.push(uppercaseFirst(pkmJson.name));
+        }
+
+    
+        
+
+        // console.log(firstPkmJson.sprites.other.official-artwork.front_default);
+    }
+    startButton.addEventListener('click', () => {
+        pokeNameBar.innerHTML = pokemonNameList[0];
+        pokemonPic.src =  pokemonPicList[0];
+
+        //Make the start button dissappear when clicked
+        startButton.remove();
+        //Make the smash and Pass buttons appear
+        smashButton.style.display = 'inline-block';
+        passButton.style.display = 'inline-block';
+        
+    })
+
+    function spliceIt(list) {
+        let temporaryName = pokemonNameList.splice(pokemonNameList[1], 1);
+        let temporaryPokemon = pokemonPicList.splice(pokemonPicList[1], 1);
+        let temporaryConcat = temporaryName.concat(temporaryPokemon)
+        list.push(temporaryConcat);
+
+    }
+
+    smashButton.addEventListener('click', () => {
+        spliceIt(smashList);
+        pokeNameBar.innerHTML = pokemonNameList[0];
+        pokemonPic.src =  pokemonPicList[0];
+    })
+
+    passButton.addEventListener('click', () => {
+        spliceIt(passList);
+        pokeNameBar.innerHTML = pokemonNameList[0];
+        pokemonPic.src =  pokemonPicList[0];
+    })
+
+    function list() {
+        console.log(pokemonPicList);
+        console.log(pokemonNameList)
+        console.log(smashList);
+        console.log(passList);
+    }
+
+    window.list = list;
+
+    fetchPokemon();
+
+
 });
+
+
+
